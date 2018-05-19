e75b4691c4254873a69c1e3f68011a0f7695cf7c
djkloop <1044032256@qq.com>
2018-05-19 15:49:23 +0800

    idea

--- new/func.go
+++ new/func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"math"
+)
 
 func eval(a, b int, op string) (int, error) {
 	switch op {
@@ -16,7 +21,6 @@ func eval(a, b int, op string) (int, error) {
 	default:
 		return 0, fmt.Errorf(
 			"error: %s",
-			op
 		)
 	}
 }
@@ -25,12 +29,36 @@ func div(a, b int) (q, r int) {
 	return a / b, a % b
 }
 
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling func %s with args " + "(%d, %d)\n", opName, a, b)
+	return op(a, b)
+}
+
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func sum(numbers ...int) int {
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
+}
+
 func main() {
 	if result, err := eval(3, 4, "x"); err != nil {
 		fmt.Println("Error:", err)
 	} else {
 		fmt.Println(result)
 	}
-	q, r := div
 
+	q, r := div(13, 3)
+	fmt.Println(q, r)
+
+	fmt.Println(apply(pow, 3, 4))
+	fmt.Println(sum(1,2,3,4,5))
 }
